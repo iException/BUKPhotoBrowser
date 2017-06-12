090e5310a613d40fdd4678aecd2ff2026b061031
Yiming Tang <yimingnju@gmail.com>
2017-06-12 17:59:20 +0800

    Update project with Xcode 8.3

--- Example/Pods/Headers/Public/BUKPhotoBrowser/BUKPhotoView.h
+++
@@ -1,1 +0,0 @@
-../../../../../Pod/Classes/BUKPhotoView.h
--- Example/Pods/Target Support Files/Pods-BUKPhotoBrowser_Tests-BUKPhotoBrowser/Pods-BUKPhotoBrowser_Tests-BUKPhotoBrowser-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BUKPhotoBrowser_Tests_BUKPhotoBrowser : NSObject
-@end
-@implementation PodsDummy_Pods_BUKPhotoBrowser_Tests_BUKPhotoBrowser
-@end
